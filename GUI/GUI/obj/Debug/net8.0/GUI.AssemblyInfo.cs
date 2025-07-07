8bf86f61cba3793435fe262f27a3458fdc9f3254
leocodingg <132714535+leocodingg@users.noreply.github.com>
2025-07-06 19:33:34 -0700

    systen assembly changes

--- GUI/GUI/obj/Debug/net8.0/GUI.AssemblyInfo.cs
+++ GUI/GUI/obj/Debug/net8.0/GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e60c7baf0ada5ead3e14f3d3a54e7f6006b020")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
